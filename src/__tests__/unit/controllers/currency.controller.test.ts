0e0244f2605674d71fccfb1a90cc43e46f3ce2e6
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-03-27 12:13:07 -0600

    chore: fix type assertion error on the array value of data

--- src/__tests__/unit/controllers/currency.controller.test.ts
+++ src/__tests__/unit/controllers/currency.controller.test.ts
@@ -29,7 +29,7 @@ describe('Currency Controller', () => {
   const nextFunction = vi.fn();
   let responseObject: ResponseObject = {
     statusCode: 0,
-    body: {},
+    body: { data: {} },
   };
 
   beforeEach(() => {
@@ -38,7 +38,7 @@ describe('Currency Controller', () => {
     };
     responseObject = {
       statusCode: 0,
-      body: {},
+      body: { data: {} },
     };
     mockResponse = {
       status: vi.fn().mockImplementation((code) => {
@@ -169,6 +169,12 @@ describe('Currency Controller', () => {
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(responseObject.body.success).toBe(true);
       expect(responseObject.body.data).toBeInstanceOf(Array);
+      
+      // Add type guard to assert it's an array
+      if (!Array.isArray(responseObject.body.data)) {
+        throw new Error('responseObject.body.data is not an array');
+      }
+      
       expect(responseObject.body.data.length).toBeGreaterThan(0);
       expect(responseObject.body.data[0]).toHaveProperty('code');
       expect(responseObject.body.data[0]).toHaveProperty('name');
